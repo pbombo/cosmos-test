7d68ecb55f001814a869a62e257820d45202b74c
Paul Bombo <pbombo@gmail.com>
2024-09-09 19:53:47 +0300

    fix:

--- dbt/northwind/models/orders_per_month.sql
+++ dbt/northwind/models/orders_per_month.sql
@@ -7,9 +7,7 @@
 monthly_orders as (
     SELECT 
         COUNT(order_id) as order_count,
-        customer_id,
-        DATEPART(MONTH, date_add('day', order_date, date '1970-01-01')) AS month
-        
+        MONTH(date_add('day', order_date, date '1970-01-01')) AS MONTH
     FROM icerest.ingestion.cdc_northwind_public_orders
 )
 
